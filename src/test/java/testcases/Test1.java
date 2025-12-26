f350c91df0f30fb02e43b0c23a85280cb4a913ad
Omkar <omi8966@gmail.com>
2025-12-27 00:38:07 +0530

    first commit

---
+++ src/test/java/testcases/Test1.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void Test1case() {
+		System.out.println("Testcase 1");
+	}
+	@Test
+	public void Testcase2() {
+		System.out.println("Testcase 2");
+	}
+
+
+}

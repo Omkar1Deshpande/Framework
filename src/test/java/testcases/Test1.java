b74fdf92d250365bedb79d24aa740bdf7f996fde
Omkar <omi8966@gmail.com>
2025-12-27 04:26:39 +0530

    Change CICD

--- src/test/java/testcases/Test1.java
+++ src/test/java/testcases/Test1.java
@@ -1,11 +1,15 @@
 package testcases;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
 	@Test
 	public void Test1case() {
 		System.out.println("Testcase 1");
+		Assert.assertEquals("String", "NoString", "Titles of the website do not match");
 	}
 	@Test
 	public void Testcase2() {

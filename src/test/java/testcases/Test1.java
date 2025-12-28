247fc3fd25b904505de80697f282a0835dfc0e83
Omkar <omi8966@gmail.com>
2025-12-28 17:36:15 +0530

    New commit

--- src/test/java/testcases/Test1.java
+++ src/test/java/testcases/Test1.java
@@ -1,15 +1,21 @@
 package testcases;
 
-import static org.testng.Assert.assertEquals;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+
 public class Test1 {
 	@Test
+	@Description("Testcase 1")
 	public void Test1case() {
+		Allure.step("Print String");
 		System.out.println("Testcase 1");
-		Assert.assertEquals("String", "NoString", "Titles of the website do not match"
+		
+		Allure.step("Validate String");
+		Assert.assertTrue(false);
 	}
 	@Test
 	public void Testcase2() {
